19dfef102ca48f295cc3b003161ec2cdd6d89334
aswordlessninja <aswordlessninja@gmail.com>
2018-04-05 15:52:06 -0600

    Implemented some of reader method

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,25 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
-    // class to do stuff
+
+
+    public void readFile() {
+        Path f = Paths.get("/input/adj_matrix.txt");
+        Charset charset = Charset.forName("US-ASCII");
+        try (BufferedReader r = Files.newBufferedReader(f, charset)) {
+            String line = null;
+            int[] k;
+            while ((line = r.readLine()) != null) {
+                String[] csv = line.split("[,\\s]+"); // Reads line and converts it splits it at the commas
+            }
+        } catch (IOException e) {
+            System.err.format("IOException %s%n", e);
+        }
+    }
 }
