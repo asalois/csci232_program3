de8a3361af95ae2fb9f8ff8fe567f0df5baff696
aswordlessninja <aswordlessninja@gmail.com>
2018-04-05 17:46:24 -0600

    meh

--- src/Graphs.java
+++ src/Graphs.java
@@ -2,25 +2,25 @@ import java.util.PriorityQueue;
 
 public class Graphs {
 
-    public Graphs(){
+    public Graphs() {
 
     }
 
-    public void Prims(int inArray[][]){
+    public void Prims(int inArray[][]) {
         PriorityQueue<String> pQ = new PriorityQueue<String>();
-        for(int i = 0; i < inArray.length; i++){
-            for(int j = 0; j < inArray[i].length; j++){
-                if(inArray[i][j] != 0){
-                    System.out.println(getEdge(i,j));
+        for (int i = 0; i < inArray.length; i++) {
+            for (int j = 0; j < inArray[i].length; j++) {
+                if (inArray[i][j] != 0) {
+                    System.out.println(getEdge(i, j));
                 }
             }
         }
     }
 
-    public String getEdge(int i, int j){
+    public String getEdge(int i, int j) {
         String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String out = alpha.substring(i,i+1);
-        out.concat(alpha.substring(j, j +1));
+        String out = alpha.substring(i, i + 1);
+        out += alpha.substring(j, j + 1);
         return out;
     }
 }
