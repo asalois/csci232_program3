4100c23976c6185e91491615ffcdefd915674616
aswordlessninja <aswordlessninja@gmail.com>
2018-04-05 16:16:38 -0600

    mo reader

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,28 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
-
+    private int dims;
 
     public void readFile() {
         Path f = Paths.get("/input/adj_matrix.txt");
         Charset charset = Charset.forName("US-ASCII");
         try (BufferedReader r = Files.newBufferedReader(f, charset)) {
             String line = null;
-            int[] k;
+            int k = 0;
+            String[][] s = new String[dims][dims];
             while ((line = r.readLine()) != null) {
+                int i;
                 String[] csv = line.split("[,\\s]+"); // Reads line and converts it splits it at the commas
+                for (i = 0; i < dims; i ++) {
+                    s[k][i] = csv[i];
+                }
+                ++k;
+            }
+            for(int j = 0; j < dims; j++){
+                for(int m = 0; m < dims; m++){
+                    System.out.print(s[j][m]);
+                }
+                System.out.println();
             }
         } catch (IOException e) {
             System.err.format("IOException %s%n", e);
