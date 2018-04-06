909a0c6bd940295026e5d81d044caaf347faed5f
Alex Salois <corvettelover1997@gmail.com>
2018-04-05 18:08:47 -0600

    added inTree

--- src/Graphs.java
+++ src/Graphs.java
@@ -8,12 +8,11 @@ public class Graphs {
 
     public void Prims(int inArray[][]) {
         PriorityQueue<Edge> pQ = new PriorityQueue<Edge>();
+        int j = 0;
         for (int i = 0; i < inArray.length; i++) {
-            for (int j = 0; j < inArray[i].length; j++) {
-                if (inArray[i][j] != 0) {
-                    String edge = getEdge(i, j);
-                    pQ.add(new Edge(edge, i, j, inArray[i][j]));
-                }
+            if (inArray[i][j] != 0) {
+                String edge = getEdge(i, j);
+                pQ.add(new Edge(edge, i, j, inArray[i][j]));
             }
         }
     }
@@ -26,21 +25,27 @@ public class Graphs {
     }
 }
 
-class Edge implements Comparable<Edge>{
+class Edge implements Comparable<Edge> {
     public String edge;
     public int up;
     public int down;
     public int weight;
+    public boolean inTree;
 
-    public int compareTo(Edge that){
+    public int compareTo(Edge that) {
         return (int) this.weight - that.weight;
     }
 
 
-    public Edge(String e, int u, int d, int w){
+    public Edge(String e, int u, int d, int w) {
         edge = e;
         up = u;
         down = d;
         weight = w;
+        inTree = false;
+    }
+
+    public void toTrue(){
+        inTree = true;
     }
 }
